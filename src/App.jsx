3fb02a0a37205e891ffa8513914e4183be29e0ce
Santoshm <santoshm@Trinesiss-MacBook-Air.local>
2024-04-01 12:58:47 +0530

    create a signup and login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </>
   );
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,42 +1,73 @@
 import { useState } from "react";
 
 const Signup = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const[fullname, setFullname] = useState();
 
-  const[email, setEmail] = useState();
-  const[password, setPassword] = useState();
-  // const[fullname, setFullname] = useState();
-
-  function createAccount(){
+  function createAccount() {
     console.log("account created");
   }
 
-  return(
+  return (
     <div className="container">
-    <div className="form">
-      <form onSubmit={(e) => {e.preventDefault(); createAccount()}}>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <br />
-          <input type="email" name="email" value={email} onChange={(e) => {setEmail(e.target.value)} } required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <br />
-          <input type="password" name="password" value={password} onChange={(e) => {setPassword(e.target.value)} } required />
-        </div>
-        <div className="login-btn" style={{gap: "20px"}}>
-          <br />
-          <button type="createAccount" className="btn login-btn">
-            Login
-          </button>
-          <button className="btn signup-btn">
-            Signup
-          </button>
-        </div>
-      </form>
+      <div className="form">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            createAccount();
+          }}
+        >
+          <div>
+            <label htmlFor="fullname">Full Name:</label>
+            <br />
+            <input
+              type="text"
+              name="fullname"
+              value={fullname}
+              onChange={(e) => {
+                setFullname(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="login-btn" style={{ gap: "20px" }}>
+            <br />
+            <button type="createAccount" className="btn login-btn">
+              Signup
+            </button>
+            <button className="btn signup-btn">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
